5e4bc0509ff16e55c70996a1c62521c81c6e7d57
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-12 23:51:37 +0530

    Lesson 2

---
+++ Tutorial 2/Tutorial2.java
@@ -0,0 +1,11 @@
+// we should save our java file, main method's class name
+//by using the public keyword, we can access the properties of the Tutorial2 class in another class
+public class Tutorial2 {
+    //Methods
+    //Main method
+    public static void main(String args[]) { //void mean not return any value out of this method
+               System.out.println("Hello World 2"); //System is a predefine class in java. 
+
+    }
+}
+
