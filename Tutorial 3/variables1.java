76ed1378bd9919091d5c2cc79118061e058fef5b
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-13 00:15:03 +0530

    Lesson 3

---
+++ Tutorial 3/variables1.java
@@ -0,0 +1,10 @@
+
+public class variables1 {
+   public static void main(String args[]) {
+    int number = 10; //create a variable called number and assign it to 10
+    System.out.println("number: " + number); //print the number
+    System.out.print("number: "); //use print key word for print value separate lines
+    System.out.print(number); 
+
+   }
+}
