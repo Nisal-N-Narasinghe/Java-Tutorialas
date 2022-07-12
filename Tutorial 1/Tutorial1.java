3b0c4a4d86f64958fc505d24e8bc92e7f1d21ede
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-12 23:06:40 +0530

    Lesson 1

---
+++ Tutorial 1/Tutorial1.java
@@ -0,0 +1,11 @@
+//create class by using file name
+public class Tutorial1 {
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!"); //Display Hello World
+    }
+}
+/* First go to this Tutorial 1 folder then press shift + rightclick and click on open in terminal. 
+Next type in javac Tutorial1.java and press enter. 
+Then you can see Tutorial1.class file.After that type java Tutorial1 in terminal and press enter.
+Now we can see the output "Hello World!" in terminal. */
