cd3f2e2675b39d7fa15c9fd57795f330f0d745d7
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-13 16:40:47 +0530

    Lesson 8

---
+++ Tutorial 8/more_with_scanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class more_with_scanner {
+    public static void main(String args[]) {
+
+        Scanner num = new Scanner(System.in);
+        int number = num.nextInt();
+        System.out.println("Number: " + number);
+    }
+}
