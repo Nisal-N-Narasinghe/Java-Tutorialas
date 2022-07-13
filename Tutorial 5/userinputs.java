906f886952b66d6a771d325703fee7ba8c8a26ff
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-13 11:23:31 +0530

    Lesson 5

--- Tutorial 5/userinputs.java
+++ Tutorial 5/userinputs.java
@@ -5,10 +5,21 @@ import java.util.Scanner; // import scanner to getting user inputs
 public class userinputs {
     public static void main(String[] args) {
         //create scanner variable
-        Scanner animals = new Scanner(System.in); //"animals" is a random variable name
+        Scanner animals = new Scanner(System.in);
+        
+            //System.out.println(animals.next()); //print first word wiche user input
+            System.out.println(animals.nextLine()); //Print whole line wiche user input
 
-        //System.out.println(animals.next()); //print first word wiche user input
-        System.out.println(animals.nextLine()); //Print whole line wiche user input
+        /* //This methos not sure but it removes all errors
+        try (Scanner animals = new Scanner(System.in)) 
+        {
+            //System.out.println(animals.next()); //print first word wiche user input
+            System.out.println(animals.nextLine()); //Print whole line wiche user input
+        }
+         */
+
+        
+        
     }
 
 }
