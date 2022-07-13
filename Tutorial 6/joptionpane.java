a53334b4c75cbb9b9c7e9357a6418234ffd25e7a
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-13 15:51:30 +0530

    Lesson 6

--- Tutorial 6/joptionpane.java
+++ Tutorial 6/joptionpane.java
@@ -1,3 +1,12 @@
+import javax.swing.JOptionPane;
+
 public class joptionpane {
     
+    
+    public static void main(String args[]) {
+        
+            String name = JOptionPane.showInputDialog("Hi , My name is Acer. What is your name?");
+
+            JOptionPane.showMessageDialog(null, "I love you " + name);
+    }
 }
