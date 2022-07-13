2399f2ba0b217e7bc14d114447c6c2fcbbc9a61b
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-13 10:52:41 +0530

    Lesson 4

---
+++ Tutorial 4/variables2.java
@@ -0,0 +1,11 @@
+public class variables2 {
+    public static void main(String args[]) {
+        double num;
+        
+        num = 10.3;
+        System.out.println(num);
+        
+        num = 22/7.0;
+        System.out.println(num);
+    }
+}
