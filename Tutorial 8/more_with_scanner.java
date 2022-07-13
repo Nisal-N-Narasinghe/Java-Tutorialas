7853838554c2ab0c293ab9478eafb2c6fd9318e1
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-13 16:55:20 +0530

    Lesson 8

--- Tutorial 8/more_with_scanner.java
+++ Tutorial 8/more_with_scanner.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class more_with_scanner {
     public static void main(String args[]) {
 
-        Scanner num = new Scanner(System.in);
-        int number = num.nextInt();
-        System.out.println("Number: " + number);
+        Scanner myvari = new Scanner(System.in); // create scanner variable
+        // int number = myvari.nextInt(); //assign scanner variable value to number this
+        // method use if user input is integer
+        // float number = num.nextFloat(); //assign scanner variable value to number
+        // this method use if user input is float
+        String name = myvari.nextLine(); // assign scanner variable value to name variable this method use if user input
+                                         // is string
+        // System.out.println("Number: " + number);
+        System.out.println("My name is " + name);
     }
 }
