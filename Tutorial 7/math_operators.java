542b53595d6d51147b9243cac80e3213e39d35e4
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-13 16:11:57 +0530

    Lesson 7

---
+++ Tutorial 7/math_operators.java
@@ -0,0 +1,12 @@
+public class math_operators {
+
+    public static void main(String args[]) {
+        double num1, num2, ans;
+        num1 = 10;
+        num2 = 28;
+
+        ans = num1 + num2;
+        System.out.println(ans);
+
+    }
+}
