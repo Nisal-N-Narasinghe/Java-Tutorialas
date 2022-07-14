6b6e63b9e1cfbc6ceb311c4c283df843ad9caefb
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-14 23:32:43 +0530

    Lesson 11

--- Tutorial 11/if_else.java
+++ Tutorial 11/if_else.java
@@ -1,18 +1,33 @@
 
+import javax.swing.JOptionPane;
+//import java.util.Scanner;
+
 public class if_else {
     public static void main(String[] args) {
-        int num = 5;
+        String name = JOptionPane.showInputDialog("Enter your Name: ");
+        String sci = JOptionPane.showInputDialog("Enter your science marks: ");
+        float science = Float.parseFloat(sci);
+        String mth = JOptionPane.showInputDialog("Enter your maths marks: ");
+        float maths = Float.parseFloat(mth);
+        // System.out.println("Enter your science marks: ");
+        // Scanner sci = new Scanner(System.in);
+        // System.out.println("Enter your maths marks: ");
+        // Scanner mth = new Scanner(System.in);
+        // float science = sci.nextFloat();
+        // float maths = mth.nextFloat();
 
-        if (num == 10) {
-            System.out.println("Number equal to 10");
-        } else if (num != 10) {
-            System.out.println("Number not equal to 10");
-        } else if (num > 10) {
-            System.out.println("Number greater than 10");
-        } else if (num >= 10) {
-            System.out.println("Number greater than or equal to 10");
-        } else if (num <= 10) {
-            System.out.println("YES");
+        if (maths >= 75 && science >= 75) {
+            JOptionPane.showMessageDialog(null, name + " your Grade is A");
+        } else if (maths >= 65 && science >= 65) {
+            JOptionPane.showMessageDialog(null, name + " your Grade is B");
+        } else if (maths >= 55 && science >= 55) {
+            JOptionPane.showMessageDialog(null, name + " your Grade is C");
+        } else if (maths >= 45 && science >= 45) {
+            JOptionPane.showMessageDialog(null, name + " your Grade is D");
+        } else if ((maths < 45 && maths > 0 && science < 45 && science > 0)) {
+            JOptionPane.showMessageDialog(null, name + ", uba fail machn");
+        } else {
+            JOptionPane.showMessageDialog(null, name + " Invalid Input.. Please enter valid marks..");
         }
 
     }
