3238d4001fb263963b4d622649036496c701672c
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-14 12:02:07 +0530

    Lesson 10

---
+++ Myfirstproject/src/main/java/com/mycompany/myfirstproject/Myfirstproject.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.myfirstproject;
+
+public class Myfirstproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
