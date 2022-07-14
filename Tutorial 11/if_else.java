2fc3802543197d126fa0cb57051ff943b0c49205
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-14 22:28:07 +0530

    Lesson 11

--- Tutorial 11/if_else.java
+++ Tutorial 11/if_else.java
@@ -1,5 +1,19 @@
+
 public class if_else {
     public static void main(String[] args) {
+        int num = 5;
+
+        if (num == 10) {
+            System.out.println("Number equal to 10");
+        } else if (num != 10) {
+            System.out.println("Number not equal to 10");
+        } else if (num > 10) {
+            System.out.println("Number greater than 10");
+        } else if (num >= 10) {
+            System.out.println("Number greater than or equal to 10");
+        } else if (num <= 10) {
+            System.out.println("YES");
+        }
 
     }
 
