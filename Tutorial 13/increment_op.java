a2478f214e9dcad1cda024bcd42a3446c770f93c
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-16 00:11:29 +0530

    Lesson 13

---
+++ Tutorial 13/increment_op.java
@@ -0,0 +1,13 @@
+public class increment_op {
+
+    public static void main(String[] args) {
+        int apple = 5;
+
+        System.out.println(++apple);
+        System.out.println(apple++);
+        System.out.println(apple);
+        System.out.println(--apple);
+        System.out.println(apple--);
+        System.out.println(apple);
+    }
+}
