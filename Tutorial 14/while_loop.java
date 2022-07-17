7dc184e2acf72563019ff5fec9eb4f7fbdcdb6dc
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-17 13:22:37 +0530

    Lesson 14

---
+++ Tutorial 14/while_loop.java
@@ -0,0 +1,11 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 5) {
+            System.out.println("Nisal");
+            System.out.println(count);
+            count++;
+        }
+    }
+}
