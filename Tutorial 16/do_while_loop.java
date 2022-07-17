c12b647d5db19fc35a099cf4dec7e6245693b5b4
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-17 13:51:33 +0530

    Lesson 16

---
+++ Tutorial 16/do_while_loop.java
@@ -0,0 +1,11 @@
+public class do_while_loop {
+    public static void main(String[] args) {
+        int i = 11;
+
+        do {
+            System.out.println(i + " do_while_loop");
+            i++;
+
+        } while (i <= 10);
+    }
+}
