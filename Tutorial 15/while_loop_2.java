b40bb164105e0fdb58eaa1a51c75370d18f65040
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-17 13:42:56 +0530

    Lesson 15

---
+++ Tutorial 15/while_loop_2.java
@@ -0,0 +1,32 @@
+public class while_loop_2 {
+    public static void main(String[] args) {
+        // int count = 1;
+        // int i = 10;
+        int j = 1;
+
+        /*
+         * //2 by 2 loop
+         * while (count <= 10) {
+         * 
+         * System.out.println(count);
+         * 
+         * count += 2; // Count 2 by 2
+         * }
+         */
+
+        /*
+         * // Count decrement
+         * while (i > 0) {
+         * System.out.println("Hello World");
+         * i--;
+         * }
+         */
+
+        // Print value with number
+        while (j <= 5) {
+            System.out.println(j + " Hi Nisal");
+            j++;
+        }
+
+    }
+}
