01c4b8f455f9156c58315a2ba5dd6f09142e35bb
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-19 23:27:39 +0530

    Lesson 17

---
+++ Tutorial 17/for_loop.java
@@ -0,0 +1,10 @@
+public class for_loop {
+    public static void main(String[] args) {
+        int i = 1;
+
+        for (i = 1; i < 10; i++) {
+            System.out.println(i + " Hello world");
+        }
+
+    }
+}
