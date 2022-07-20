648c41b5290c66f2209a2e95b9d958b599bd1aa6
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-20 23:58:34 +0530

    Lesson 23

---
+++ Tutorial 23(Conditional Operators)/apple.java
@@ -0,0 +1,9 @@
+public class apple {
+
+    public static void main(String[] args) {
+        int age = 22;
+
+        System.out.println(age < 50 ? "You are are Young" : "You are not Young");
+    }
+
+}
