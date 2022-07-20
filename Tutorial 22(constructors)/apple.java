b0f5447a19dfefbd31307303d01284fc0218f3f0
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-20 23:47:14 +0530

    Lesson 22

--- Tutorial 22(constructors)/apple.java
+++ Tutorial 22(constructors)/apple.java
@@ -5,12 +5,14 @@ public class apple {
 
     public static void main(String[] args) {
 
-        banana bananaObj = new banana();
+        String yourName = JOptionPane.showInputDialog("Enter your Name: ");
+        String yourFood = JOptionPane.showInputDialog("Enter your favourite food: ");
 
-        String yourfood = JOptionPane.showInputDialog("Enter your favourite food: ");
+        banana bananaObj = new banana(yourName, yourFood);
 
-        bananaObj.getFoodName(yourfood);
+        // bananaObj.getFoodName(yourfood);
         bananaObj.displayName();
+
     }
 
 }
--- Tutorial 22(constructors)/banana.java
+++ Tutorial 22(constructors)/banana.java
@@ -2,21 +2,35 @@ import javax.swing.JOptionPane;
 
 public class banana {
 
+    private String name; // create a private variable for the Name
     private String foodName; // create a private variable for the food name
 
-    // creat a method to get the food name
-    public void getFoodName(String fdname) {
-        foodName = fdname; // assign the food name to the private variable
+    // create constructor
+    public banana(String pname, String fdName) {
+        name = pname; // assign the name to the private variable
+        foodName = fdName; // assign the food name to the private variable
+    }
+
+    /*
+     * // creat a method to get the food name
+     * public void getFoodName(String fdname) {
+     * foodName = fdname; // assign the food name to the private variable
+     * }
+     */
+
+    // create a method to return the name
+    public String setName() {
+        return name;
     }
 
     // create a method to return the food name
-    public String setName() {
+    public String setFoodName() {
         return foodName;
     }
 
     // create a method to display the food name
     public void displayName() {
-        JOptionPane.showMessageDialog(null, "Your favourite food is " + setName());
+        JOptionPane.showMessageDialog(null, setName() + " your favourite food is " + setFoodName());
         // System.out.println("Your favourite food is " + setName());
     }
 
