b357a4c582b6d0a20caaef6487d9902050373ad5
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-21 13:11:15 +0530

    Lesson 25

--- Tutorial 25(randome number generator)/apple.java
+++ Tutorial 25(randome number generator)/apple.java
@@ -9,12 +9,29 @@ public class apple {
 
         int number; // ceate variable for get numbers from generator
 
+        System.out.println("Generate numbers ");
         // "forloop" for how many times to generate random numbers
         for (int i = 0; i < 10; i++) {
 
+            /*
+             * getting random number bellow 5 from generator and assign it to number
+             * variable
+             */
             number = rand.nextInt(5);
+            System.out.println(number);// display the values assigned to the number variable
 
-            System.out.println(number);
+        }
+
+        System.out.println(" ");
+        System.out.println("Generate number = number + 1 ");
+        for (int j = 0; j < 10; j++) {
+
+            /*
+             * getting random number bellow 5 from generator and assign it to
+             * number = number + 1
+             */
+            number = 1 + rand.nextInt(5);
+            System.out.println(number);// display the values assigned to the number variable
 
         }
     }
