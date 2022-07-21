6788bd972084249bc01857402c8333c1169bef5a
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-21 11:57:44 +0530

    Lesson 24

--- Tutorial 24(math class)/apple.java
+++ Tutorial 24(math class)/apple.java
@@ -1,6 +1,12 @@
 public class apple {
     public static void main(String[] args) {
 
-        System.out.println(Math.abs(-32.8));
+        System.out.println(Math.abs(-32.8)); // display the absolute value of the -32.8
+        System.out.println(Math.pow(2, 3)); // display the value of the 2*2*2
+        System.out.println(Math.sqrt(25)); // display the squreroot of the 25
+        System.out.println(Math.ceil(7.2)); // display the closest maximum value
+        System.out.println(Math.floor(7.2)); // display the closest minimum value
+        System.out.println(Math.max(7, 13.8)); // display the maximum value of the given values
+        System.out.println(Math.min(7, 13.8)); // display the minimum value of the given values
     }
 }
