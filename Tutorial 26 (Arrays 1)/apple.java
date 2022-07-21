c0c0ed95a4eafd9c4d8f14f5734664f6c5566050
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-21 13:28:07 +0530

    Lesson 26

--- Tutorial 26 (Arrays 1)/apple.java
+++ Tutorial 26 (Arrays 1)/apple.java
@@ -1,7 +1,25 @@
 public class apple {
 
     public static void main(String[] args) {
-        int num[] = new int[5];
+
+        /*
+         * // First method to create an array
+         * int num[] = new int[5];
+         * 
+         * num[0] = 224;
+         * num[1] = 324;
+         * num[2] = 244;
+         * num[3] = 264;
+         * num[4] = 824;
+         * 
+         * System.out.println(num[4]);
+         */
+
+        // Second method to create an array
+        int num[] = { 10, 1, 22, 13, 2345 };
+
+        System.out.println(num[3]);
+
     }
 
 }
