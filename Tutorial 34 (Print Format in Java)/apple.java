fb7aef00733b4ccc30172a862a643f39abb96c90
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-08-26 12:09:51 +0530

    Lesson 34

--- Tutorial 34 (Print Format in Java)/apple.java
+++ Tutorial 34 (Print Format in Java)/apple.java
@@ -1,9 +1,9 @@
 public class apple {
     public static void main(String[] args) {
         System.out.printf("My name is %s and my age is %d and my GPA equals to %.2f",
-                "Nisal", 22, 3.432);
+                "Nisal", 22, 3.432); // pintf
 
         System.out.println();
-        System.out.printf("%03d", 2);
+        System.out.printf("%03d", 2); // %03d for gettin the numbers count
     }
 }
