87b628ca47531ca8d745c3a05961281076c210cc
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-08-26 11:40:40 +0530

    Lesson 34

---
+++ Tutorial 34 (Print Format in Java)/apple.java
@@ -0,0 +1,5 @@
+public class apple {
+    public static void main(String[] args) {
+        System.out.printf("My name is %s and my age is %d and my GPA equals to %.2f", "Nisal", 22, 3.432);
+    }
+}
