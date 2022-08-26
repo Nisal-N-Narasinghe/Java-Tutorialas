8d862768cf1291358cafdd4403e99de4c7351330
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-08-26 11:30:58 +0530

    Lesson 33

--- Tutorial 33 (Variable Length Argument)/apple.java
+++ Tutorial 33 (Variable Length Argument)/apple.java
@@ -1,5 +1,14 @@
 public class apple {
     public static void main(String[] args) {
+        secondMeth(2, 3, 4, 6, 7);
+    }
 
+    public static void secondMeth(int... numbers) {
+        int total = 0;
+
+        for (int y : numbers)
+            total += y;
+
+        System.out.println("average: " + total / numbers.length);
     }
 }
