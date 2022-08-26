c7037bfb907345a2ab56ce0bdba22b47e8a28b53
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-08-26 12:52:26 +0530

    Lesson 35

--- Tutorial 35 (Escape Sequence)/apple.java
+++ Tutorial 35 (Escape Sequence)/apple.java
@@ -3,5 +3,15 @@ public class apple {
         System.out.println("Jhon said, \"I am hungry\" "); // \" use for print "
 
         System.out.println("D:\\Github\\Java "); // use \\ for print \
+
+        // \n :- new line tag
+        System.out.println("Jhon said, \nI am hungry ");
+
+        // \r :- overwrite tag
+        System.out.println("Jhon said, \rI am hungry ");
+
+        // \t :- tabspace tag
+        System.out.println("Jhon said, \tI am hungry ");
+
     }
 }
