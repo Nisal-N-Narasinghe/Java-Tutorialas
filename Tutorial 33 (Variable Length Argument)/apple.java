d7141ca3054e4b355a4d27390ded54cef5586d4a
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-08-26 12:08:37 +0530

    Lesson 33

--- Tutorial 33 (Variable Length Argument)/apple.java
+++ Tutorial 33 (Variable Length Argument)/apple.java
@@ -1,14 +1,14 @@
 public class apple {
     public static void main(String[] args) {
-        secondMeth(2, 3, 4, 6, 7);
+        secondMeth(2, 3, 4, 6, 7); // pass value
     }
 
-    public static void secondMeth(int... numbers) {
+    public static void secondMeth(int... numbers) { // creatr variable length argument
         int total = 0;
 
-        for (int y : numbers)
+        for (int y : numbers) // for loop for calculate the total of the entered values
             total += y;
 
-        System.out.println("average: " + total / numbers.length);
+        System.out.println("average: " + total / numbers.length); // Display the average of the total
     }
 }
--- Tutorial 34 (Print Format in Java)/apple.java
+++ Tutorial 34 (Print Format in Java)/apple.java
@@ -1,5 +1,9 @@
 public class apple {
     public static void main(String[] args) {
-        System.out.printf("My name is %s and my age is %d and my GPA equals to %.2f", "Nisal", 22, 3.432);
+        System.out.printf("My name is %s and my age is %d and my GPA equals to %.2f",
+                "Nisal", 22, 3.432);
+
+        System.out.println();
+        System.out.printf("%03d", 2);
     }
 }
